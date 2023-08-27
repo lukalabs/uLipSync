b352fdb3dcf66203cf49587941fcaf7adf3f1091
a.gorozhankin <a.gorozhankin@replika.ai>
2023-08-27 15:44:51 +0300

    add volume field for audio source

--- Runtime/uLipSyncAudioSource.cs
+++ Runtime/uLipSyncAudioSource.cs
@@ -3,18 +3,21 @@
 namespace uLipSync
 {
 
-[RequireComponent(typeof(AudioSource))]
-public class uLipSyncAudioSource : MonoBehaviour
-{
-    public AudioFilterReadEvent onAudioFilterRead { get; private set; } = new AudioFilterReadEvent();
-
-    void OnAudioFilterRead(float[] input, int channels)
+    [RequireComponent(typeof(AudioSource))]
+    public class uLipSyncAudioSource : MonoBehaviour
     {
-        if (onAudioFilterRead != null) 
+        public AudioFilterReadEvent onAudioFilterRead { get; private set; } = new AudioFilterReadEvent();
+
+        [Range(0f, 1f)]
+        [SerializeField] private float volume = 1f;
+        public float Volume { get => volume; set => volume = value; }
+
+        public void OnAudioFilterRead(float[] input, int channels)
         {
             onAudioFilterRead.Invoke(input, channels);
+            if (volume == 1) return;
+            for (int i = 0; i < input.Length; i++) input[i] = input[i] * volume;
         }
     }
-}
 
 }
