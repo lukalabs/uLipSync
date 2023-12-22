34c556cff0c9d0c7535631ec45c030bcc7bcf51c
Alexander.G <alexgorozh@gmail.com>
2023-12-22 09:52:57 +0300

    fix for async problem

--- Runtime/uLipSyncAudioSource.cs
+++ Runtime/uLipSyncAudioSource.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
+using System.Collections.Generic;
 
 namespace uLipSync
 {
@@ -14,11 +15,24 @@ namespace uLipSync
         [SerializeField] private float volume = 1f;
         public float Volume { get => volume; set => volume = value; }
 
+        private Queue<float[]> queue = new();
+        private int channels;
+
         public void OnAudioFilterRead(float[] input, int channels)
         {
-            audioFilterRead.Invoke(input, channels);
+            this.channels = channels;
+            queue.Enqueue(input);
             if (volume == 1) return;
             for (int i = 0; i < input.Length; i++) input[i] = input[i] * volume;
+        }
+
+        private void Update()
+        {
+            if (queue.Count == 0) return;
+            var chunk = new List<float>(queue.Dequeue());
+            while (queue.Count > 0)
+                chunk.AddRange(queue.Dequeue());
+            audioFilterRead.Invoke(chunk.ToArray(), channels);
         }
     }
 
